c5642f38879a62dc3b1c249e5f144bb54ad0b46b
Vic_Octavian <victoctavian124@gmail.com>
2024-09-26 14:23:43 +0100

    implementing layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {fisrtName: 'Vic_Dev'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className='home-content'>
+        <div className='home-header'>
+            <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.fisrtName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently."
+            />
+            <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+            />
+        </div>
+      </div>
+    </section>
   )
 }
 
